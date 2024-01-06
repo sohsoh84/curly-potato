87290d2b810f437009bf8e1f9e44fb6a2ba950bf
Soheil Mohammadkhani <mohammadkhani.soheil@gmail.com>
2024-01-06 14:28:46 +0330

    init cmake

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "test.h"
+
+int main() {
+        printf("Yayyy! %d", yay(1, 2));
+        return 0;
+}
---
+++ src/test.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int yay(int a, int b);
+
+#endif
